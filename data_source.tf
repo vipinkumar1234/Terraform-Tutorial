dc055d31d05e1e0912053996038051c60cbdf989
vipinkumar1234 <vipinggime111@gmail.com>
2023-10-26 19:40:13 +0530

    Using Data Source in Terraform

---
+++ data_source.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  owners           = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami" {
+  value = data.aws_ami.ubuntu.id
+  
+}
+  
