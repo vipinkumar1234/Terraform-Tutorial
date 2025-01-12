4b642a621a723154d57d51639df8bcca2cbc4da6
vipinkumar1234 <vipinggime111@gmail.com>
2025-01-12 09:06:19 +0530

    Adding dynamic block

--- all/Dynamic_blocks.tf
+++ all/Dynamic_blocks.tf
@@ -2,21 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "sg_port" {
+variable "allowed_ports" {
   type = list(number)
-  description = "Allowed Ports in dynamic sg "
-  default = [ 8080, 8081, 8082, 8083, 8084]
-}  
-resource "aws_security_group" "dynamicsg-1" {
-  name = "dynamic-sg"
-  
+  default = [ 8080, 8081, 22, 443, 5050, ]
+}
+
+resource "aws_security_group" "Test-sg" {
+  name = "Youtube-test-sg"
+
   dynamic "ingress" {
-    for_each = var.sg_port
+    for_each = var.allowed_ports
     content {
       from_port = ingress.value
       to_port = ingress.value
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-    }
+    } 
   }
-}
+} 
